697137990e5f9c11363a408076a5fb71d482aaad
Aleksandra <Maplewood3300@yandex.ru>
2025-07-01 22:52:32 +0300

    first commit

---
+++ src/main/java/project/calculate/Calculate.java
@@ -0,0 +1,21 @@
+package project.calculate;
+
+public class Calculate {
+    int a ;
+    int b ;
+    int c ;
+
+    public Calculate(int a, int b, int c) {
+
+    }
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int sub(int a, int b) {
+        return a-b;
+    }
+
+
+}
---
+++ src/test/java/project/calculate/CalculateTest.java
@@ -0,0 +1,22 @@
+package project.calculate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class CalculateTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+
+    }
+}
