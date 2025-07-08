be0375df99fefed38d3ab9e2c100f7d1432f281f
Aleksandra <Maplewood3300@yandex.ru>
2025-07-08 22:54:10 +0300

    check libraries

--- src/test/java/project/calculate/CalculateTest.java
+++ src/test/java/project/calculate/CalculateTest.java
@@ -1,9 +1,7 @@
 package project.calculate;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
