fd6fcbb3ab0bd76e09f7bac46aa404ffb5d13df6
Aleksandra <Maplewood3300@yandex.ru>
2025-07-09 14:07:15 +0300

    add tests

--- src/main/java/project/calculate/Calculate.java
+++ src/main/java/project/calculate/Calculate.java
@@ -1,7 +1,5 @@
 package project.calculate;
 
-import java.text.DecimalFormat;
-
 public class Calculate {
 
     public static int sum(int a, int b) {
@@ -10,16 +8,5 @@ public class Calculate {
 
     public static int sub(int a, int b) {
         return a-b;
-    }
-
-     public static int multiply(int a, int b) {
-        return a*b;
-     }
-
-    public static Double divide(int a, int b) {
-        if (b == 0) {
-            return a == 0 ? Double.NaN : (a > 0 ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY);
-        }
-        return (double) a / b;
     }
 }
